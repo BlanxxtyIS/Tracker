344ee08022500f8e2d03cbedfc60c298367d8b06
Blanxxty <mvtvr21@yandex.ru>
2023-11-28 09:42:19 +0300

    Initial Commit

---
+++ Helpers/UIColor + Extension.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor + Extension.swift
+//  Tracker
+//
+//  Created by Марат Хасанов on 28.11.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var uBlue: UIColor { UIColor(named: "Blue") ?? UIColor.blue }
+    static var uWhite: UIColor { UIColor(named: "White [day]") ?? UIColor.white }
+}
